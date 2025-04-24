b4c38d43e91d3047c9526c7a318db49ac00def14
Radiva279 <radivagalih12@gmail.com>
2025-04-24 11:15:17 +0700

    menampilkan teks

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -7,5 +7,20 @@ int arr[20], B[20];
 int n;
 
 void input(){
+        while (true)
+        {
+            cout << "Masukkan Panjang element array : ";
+            cin >> n;
     
-}
+            if (n <= 20)
+            {
+                break;
+            }
+            else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+        cout << "\n--------------------------" << endl;
+        cout << "\nInputkan Isi element array" << endl;
+        cout << "\n--------------------------" << endl;
