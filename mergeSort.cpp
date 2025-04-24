454cae9433edf0d6dcfbd3cfbf6a1debf0835d98
Radiva279 <radivagalih12@gmail.com>
2025-04-24 11:21:11 +0700

    menambhakan perulangan while lagi

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -65,5 +65,12 @@ void mergeSort(int low, int high)
         }
         k++;
 
+        while (i <= mid) // step 4.f
+        {
+            B[k] = arr[i]; // step 4.f.i
+            i++; // step 4.f.ii
+            k++; // step 4.f.iii
+        }
+
 }
 
